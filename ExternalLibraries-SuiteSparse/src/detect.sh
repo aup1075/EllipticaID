daeac69c8757c764cb74fa7a4dc4dc125d6664a2
Pedro <pespino@email.arizona.edu>
2023-02-23 14:58:08 -0500

    added external library for compiling UMFPACK with Cactus

--- ExternalLibraries-SuiteSparse/src/detect.sh
+++ ExternalLibraries-SuiteSparse/src/detect.sh
@@ -16,17 +16,17 @@ set -e                          # Abort on errors
 # Search
 ################################################################################
 
-if [ -z "${ELLIPTICA_DIR}" ]; then
+if [ -z "${SUITESPARSE_DIR}" ]; then
     echo "BEGIN MESSAGE"
-    echo "Elliptica selected, but Elliptica_DIR not set."
+    echo "SuiteSparse selected, but SuiteSparse_DIR not set."
     echo "END MESSAGE"
 else
     echo "BEGIN MESSAGE"
-    echo "Using Elliptica in ${ELLIPTICA_DIR}"
+    echo "Using SuiteSparse in ${SUITESPARSE_DIR}"
     echo "END MESSAGE"
 fi
 
-THORN=Elliptica
+THORN=SuiteSparse
 
 
 
@@ -34,11 +34,11 @@ THORN=Elliptica
 # Build
 ################################################################################
 
-if [ -z "${ELLIPTICA_DIR}"                                                 \
-     -o "$(echo "${ELLIPTICA_DIR}" | tr '[a-z]' '[A-Z]')" = 'BUILD' ]
+if [ -z "${SUITESPARSE_DIR}"                                                 \
+     -o "$(echo "${SUITESPARSE_DIR}" | tr '[a-z]' '[A-Z]')" = 'BUILD' ]
 then
     echo "BEGIN MESSAGE"
-    echo "Using bundled Elliptica..."
+    echo "Using bundled SuiteSparse..."
     echo "END MESSAGE"
     
     # Check for required tools. Do this here so that we don't require
@@ -61,21 +61,21 @@ then
     fi
 
     # Set locations
-    NAME=Elliptica
+    NAME=SuiteSparse
     SRCDIR="$(dirname $0)"
     BUILD_DIR=${SCRATCH_BUILD}/build/${THORN}
-    if [ -z "${ELLIPTICA_INSTALL_DIR}" ]; then
+    if [ -z "${SUITESPARSE_INSTALL_DIR}" ]; then
         INSTALL_DIR=${SCRATCH_BUILD}/external/${THORN}
     else
         echo "BEGIN MESSAGE"
-        echo "Installing Elliptica into ${ELLIPTICA_INSTALL_DIR}"
+        echo "Installing SuiteSparse into ${SUITESPARSE_INSTALL_DIR}"
         echo "END MESSAGE"
-        INSTALL_DIR=${ELLIPTICA_INSTALL_DIR}
+        INSTALL_DIR=${SUITESPARSE_INSTALL_DIR}
     fi
-    ELLIPTICA_BUILD=1
-    ELLIPTICA_DIR=${INSTALL_DIR}
+    SUITESPARSE_BUILD=1
+    SUITESPARSE_DIR=${INSTALL_DIR}
 else
-    ELLIPTICA_BUILD=
+    SUITESPARSE_BUILD=
     DONE_FILE=${SCRATCH_BUILD}/done/${THORN}
     if [ ! -e ${DONE_FILE} ]; then
         mkdir ${SCRATCH_BUILD}/done 2> /dev/null || true
@@ -91,28 +91,28 @@ fi
 
 # Pass configuration options to build script
 echo "BEGIN MAKE_DEFINITION"
-echo "ELLIPTICA_BUILD          = ${ELLIPTICA_BUILD}"
-echo "ELLIPTICA_EXTRA_LIB_DIRS = ${ELLIPTICA_EXTRA_LIB_DIRS}"
-echo "ELLIPTICA_EXTRA_LIBS     = ${ELLIPTICA_EXTRA_LIBS}"
-echo "ELLIPTICA_INSTALL_DIR    = ${ELLIPTICA_INSTALL_DIR}"
+echo "SUITESPARSE_BUILD          = ${SUITESPARSE_BUILD}"
+echo "SUITESPARSE_EXTRA_LIB_DIRS = ${SUITESPARSE_EXTRA_LIB_DIRS}"
+echo "SUITESPARSE_EXTRA_LIBS     = ${SUITESPARSE_EXTRA_LIBS}"
+echo "SUITESPARSE_INSTALL_DIR    = ${SUITESPARSE_INSTALL_DIR}"
 echo "END MAKE_DEFINITION"
 
 # Set options -- TODO: CAN WE PUT UMFPACK HERE?
-ELLIPTICA_INC_DIRS="${ELLIPTICA_DIR}/Export/Include ${ELLIPTICA_DIR}/Include"
-ELLIPTICA_LIB_DIRS="${ELLIPTICA_DIR}/Lib ${ELLIPTICA_EXTRA_LIB_DIRS}"
-ELLIPTICA_LIBS="${ELLIPTICA_EXTRA_LIBS}"
+SUITESPARSE_INC_DIRS="${SUITESPARSE_DIR}/Export/Include ${SUITESPARSE_DIR}/Include"
+SUITESPARSE_LIB_DIRS="${SUITESPARSE_DIR}/Lib ${SUITESPARSE_EXTRA_LIB_DIRS}"
+SUITESPARSE_LIBS="${SUITESPARSE_EXTRA_LIBS}"
 
-ELLIPTICA_INC_DIRS="$(${CCTK_HOME}/lib/sbin/strip-incdirs.sh ${ELLIPTICA_INC_DIRS})"
-ELLIPTICA_LIB_DIRS="$(${CCTK_HOME}/lib/sbin/strip-libdirs.sh ${ELLIPTICA_LIB_DIRS})"
+SUITESPARSE_INC_DIRS="$(${CCTK_HOME}/lib/sbin/strip-incdirs.sh ${SUITESPARSE_INC_DIRS})"
+SUITESPARSE_LIB_DIRS="$(${CCTK_HOME}/lib/sbin/strip-libdirs.sh ${SUITESPARSE_LIB_DIRS})"
 
 # Pass options to Cactus
 echo "BEGIN MAKE_DEFINITION"
-echo "ELLIPTICA_DIR      = ${ELLIPTICA_DIR}"
-echo "ELLIPTICA_INC_DIRS = ${ELLIPTICA_INC_DIRS}"
-echo "ELLIPTICA_LIB_DIRS = ${ELLIPTICA_LIB_DIRS}"
-echo "ELLIPTICA_LIBS     = ${ELLIPTICA_LIBS}"
+echo "SUITESPARSE_DIR      = ${SUITESPARSE_DIR}"
+echo "SUITESPARSE_INC_DIRS = ${SUITESPARSE_INC_DIRS}"
+echo "SUITESPARSE_LIB_DIRS = ${SUITESPARSE_LIB_DIRS}"
+echo "SUITESPARSE_LIBS     = ${SUITESPARSE_LIBS}"
 echo "END MAKE_DEFINITION"
 
-echo 'INCLUDE_DIRECTORY $(ELLIPTICA_INC_DIRS)'
-echo 'LIBRARY_DIRECTORY $(ELLIPTICA_LIB_DIRS)'
-echo 'LIBRARY           $(ELLIPTICA_LIBS)'
+echo 'INCLUDE_DIRECTORY $(SUITESPARSE_INC_DIRS)'
+echo 'LIBRARY_DIRECTORY $(SUITESPARSE_LIB_DIRS)'
+echo 'LIBRARY           $(SUITESPARSE_LIBS)'
